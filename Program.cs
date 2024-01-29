b6e62fee7ab379e41d2b821aae09f731c646e197
amirhoseinsazizavareh <158080807+amirhoseinsazizavareh@users.noreply.github.com>
2024-01-29 20:55:00 +0330

    Add files via upload
    امیرحسین سازی زواره

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[3, 3];
+
+      
+        Console.WriteLine("Please enter the array numbers:");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("number [{0}, {1}]: ", i, j);
+                array[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+       
+        int largest = array[0, 0];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (array[i, j] > largest)
+                {
+                    largest = array[i, j];
+                }
+            }
+        }
+
+      
+        Console.WriteLine("The largest number in the array: " + largest);
+    }
+}
